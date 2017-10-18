1a167f6dcd7b1aadc3631944ac4a0d1eb1113bb8
Phil Haigh <phil@surfsoftconsulting.com>
2017-10-18 09:34:51 +0100

    Added script to seed vessels

---
+++ seed-vessels.sh
@@ -0,0 +1,12 @@
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "id=cv20&name=Liverpool%202018" localhost:8080/vessel
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "id=cv21&name=Unicef" localhost:8080/vessel
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "id=cv22&name=Garmin" localhost:8080/vessel
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "id=cv23&name=Hotelplanner.com" localhost:8080/vessel
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "id=cv24&name=Greenings" localhost:8080/vessel
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "id=cv25&name=Dare%20To%20Lead" localhost:8080/vessel
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "id=cv26&name=Visit%20Seattle" localhost:8080/vessel
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "id=cv27&name=Sanya%20Serenity%20Coast" localhost:8080/vessel
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "id=cv28&name=PSP%20Logistics" localhost:8080/vessel
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "id=cv29&name=Qingdao" localhost:8080/vessel
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "id=cv30&name=GREAT%20Britain" localhost:8080/vessel
+curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "id=cv31&name=Nasdaq" localhost:8080/vessel
